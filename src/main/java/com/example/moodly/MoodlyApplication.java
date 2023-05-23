ac5e4e90b0b088c979c48f020f7a02fc450465ca
Stéphane Eveillé <stephane.eveille-epita@outlook.fr>
2023-05-23 09:53:18 +0200

    Initial commit

---
+++ src/main/java/com/example/moodly/MoodlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.moodly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoodlyApplication.class, args);
+    }
+
+}
